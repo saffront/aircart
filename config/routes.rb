d09c48eb28ccf3b6952134feeaca42d4f988665e
saffront <jiashern@hotmail.com>
2016-03-01 15:10:21 +0000

    added about page routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'posts/new'
+  get '/about' => 'home#about'
 
 devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :posts do
